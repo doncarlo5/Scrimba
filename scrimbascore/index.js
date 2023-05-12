248cfa242d1f3dcde82de7f152e5f15bdbe4b25d
Julien THOMAS <juliien.tthomas@gmail.com>
2023-05-12 12:06:03 +0200

    New version

---
+++ scrimbascore/index.js
@@ -0,0 +1,15 @@
+let homescore = 0;
+document.getElementById("homescorescreen").textContent = homescore;
+
+let guestscore = 0;
+document.getElementById("guestscorescreen").textContent = guestscore;
+
+function homefunction(score) {
+  homescore += score;
+  document.getElementById("homescorescreen").textContent = homescore;
+}
+
+function guestbutton(score) {
+  guestscore += score;
+  document.getElementById("guestscorescreen").textContent = guestscore;
+}
